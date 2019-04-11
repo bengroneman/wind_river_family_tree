339edc8886d5c1df601a095cd8ba39e9e423f6fc
Ben Groneman <bluegroneman10@gmail.com>
2019-04-10 21:48:36 -0600

    feat: reads csv data and prints to console (WIP)

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QWidget, QDialog, QApplication, QInputDialog, QLineE
 from PyQt5.QtGui import QIcon
 from tree import Ui_Dialog
 
+import csv
+
 class AppWindow(QDialog, QWidget):
     def __init__(self):
         super().__init__()
@@ -14,18 +16,30 @@ class AppWindow(QDialog, QWidget):
         self.ui.setupUi(self)
         self.setWindowTitle(self.title)
         self.ui.toolButton.clicked.connect(self.openFileNameDialog)
-        #self.openFileNameDialog()
-        #self.openFileNamesDialog()
-        #self.saveFileDialog()
         self.show()  
 
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;CSV Files (*.csv)", options=options)
         if fileName:
             print(fileName)
+            self.get_csv_data(fileName)
+
+
+    def get_csv_data(self, file_name):
+        with open(file_name, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}') # First Name, Last Name, Family Name, Relationship, Relationship to
+                    line_count += 1
+                print(f'\t{row["First Name"]} | {row["Last Name"]} | {row["Family Name"]} | {row["Relationship"]} | {row["Relationship to"]} ')
+                line_count += 1
+            print(f'Processed {line_count} lines.')
+            print(csv_reader)
 
 
     def openFileNamesDialog(self):
@@ -33,7 +47,7 @@ class AppWindow(QDialog, QWidget):
         options |= QFileDialog.DontUseNativeDialog
         files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         if files:
-             print(files)
+            print(files)
 
 
     def saveFileDialog(self):
