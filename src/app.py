222db72f4b20deaea8acbfba3847b8eb79eec775
Ben Groneman <bluegroneman10@gmail.com>
2019-04-10 20:47:38 -0600

    feat: file... button will now open a file dialog (WIP)

--- src/app.py
+++ src/app.py
@@ -1,13 +1,48 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QWidget, QDialog, QApplication, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
 from tree import Ui_Dialog
 
-class AppWindow(QDialog):
+class AppWindow(QDialog, QWidget):
     def __init__(self):
         super().__init__()
+        self.title = 'Wind River Family Tree'
+        self.initUI()
+
+    def initUI(self):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.setWindowTitle(self.title)
+        self.ui.toolButton.clicked.connect(self.openFileNameDialog)
+        #self.openFileNameDialog()
+        #self.openFileNamesDialog()
+        #self.saveFileDialog()
         self.show()  
+
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+
+
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        if files:
+             print(files)
+
+
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
+
 
 app = QApplication(sys.argv)
 w = AppWindow()
--- src/tree.py
+++ src/tree.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'tree.ui'
+# Form implementation generated from reading ui file 'src/tree.ui'
 #
-# Created by: PyQt5 UI code generator 5.12.1
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.resize(1117, 668)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(290, 20, 81, 241))
+        self.buttonBox.setGeometry(QtCore.QRect(990, 50, 81, 81))
         self.buttonBox.setOrientation(QtCore.Qt.Vertical)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Apply|QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
+        self.tableView = QtWidgets.QTableView(Dialog)
+        self.tableView.setGeometry(QtCore.QRect(0, 110, 941, 571))
+        self.tableView.setObjectName("tableView")
+        self.toolButton = QtWidgets.QToolButton(Dialog)
+        self.toolButton.setGeometry(QtCore.QRect(890, 50, 51, 41))
+        self.toolButton.setObjectName("toolButton")
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
@@ -27,5 +32,5 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-
+        self.toolButton.setText(_translate("Dialog", "...File"))
 
